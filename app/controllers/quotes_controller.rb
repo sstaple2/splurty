96b8fdfec388305e768097dcbca7bc9bd1c927fc
Scott Stapley <scott@bigscoots.com>
2016-11-30 23:38:27 +0000

    finished

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,7 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end 
-  def new
-        @quote = Quote.new
-  end
+
 
   def create
       @quote = Quote.create(quote_params)
@@ -15,7 +13,7 @@ class QuotesController < ApplicationController
   end
 
 def   about
-  
+
 end
 
 
