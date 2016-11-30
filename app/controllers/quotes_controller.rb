8efaea4f760b82341639e564ba5801c848c4fa63
Scott Stapley <scott@bigscoots.com>
2016-11-30 22:35:41 +0000

    mongol

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,4 +5,24 @@ class QuotesController < ApplicationController
   def new
         @quote = Quote.new
   end
+
+  def create
+      @quote = Quote.create(quote_params)
+      if @quote.invalid?
+          flash[:error] = '<strong>Could not save</strong> the data you entered.'
+      end
+      redirect_to root_path
+  end
+
+def   about
+  
+end
+
+
+    private
+
+    def quote_params
+        params.require(:quote).permit(:saying, :author)
+    end
+
 end
